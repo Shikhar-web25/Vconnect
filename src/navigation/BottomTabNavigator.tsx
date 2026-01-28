c09a4dba36c81246f03da881ca0ddd1bd082d562
ARYAN KATWAL <katwal.aryan11@gmail.com>
2026-01-28 21:48:43 +0530

    change

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
 import HomeScreen from '../screens/HomeScreen';
 import DmsScreen from '../screens/DmsScreen';
 import DiscoverScreen from '../screens/DiscoverScreen';
